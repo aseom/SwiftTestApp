f85bc23fb0cdfd8ec3670ff1c405878c3e4183a0
aseom <hm9599@gmail.com>
2016-04-28 21:44:28 +0900

    Initial Commit

---
+++ SwiftTestApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftTestApp
+//
+//  Created by ASeom on 2016. 4. 28..
+//  Copyright © 2016년 ASeom. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
